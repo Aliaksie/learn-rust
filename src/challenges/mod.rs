f607dca8658f8c7cb2f42b9119135ee9e038593e
a.darafeyeu <Aliaksei_Darafeyeu@epam.com>
2023-01-05 14:24:39 +0100

    adds more challenges

--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -3,3 +3,6 @@ pub mod find;
 pub mod morse_code;
 pub mod black_jack;
 pub mod isbn;
+pub mod run_length;
+pub mod travel_planner;
+pub mod vigenere;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod challenges;
 
 fn main() {
-    let rust_in_action: challenges::isbn::Isbn = "978-3-16-148410-0".parse().unwrap();
+    let key = "WHYRUST";
+    let ciphertext = "
+    PVCDJG
+    PAYCMY
+    JRKUC
+    ";
+    let plaintext = challenges::vigenere::decrypt(&ciphertext, key);
 
-    println!("Rust in Action's ISBN-13 ({})is valid!", rust_in_action);
+    println!("Plaintext {}", plaintext);
+    println!("Ciphertext {}", challenges::vigenere::encrypt(&plaintext, key))
 }
